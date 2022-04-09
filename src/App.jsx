3f232eef798044a0e0e5fdc94aa128fda7872f02
MatheusFontenele <matheus.fontenele@icloud.com>
2022-04-09 11:16:38 -0300

    feat: ensure the connection to firestore and return data

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,22 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 import "./App.css";
-import locomotiveScroll from "locomotive-scroll";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
 
 function App() {
-  // const scrollRef = useRef();
-  // const scrollFixed = useRef();
-  // const scrollSection = useRef();
-  // const ghost = useRef();
-  const container = useRef();
-
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.defaults({ ease: "none", duration: 3 });
-
-    const timeline = gsap.timeline();
-
-    timeline.from(".div2", { xPercent: -100 });
-    timeline.from(".div3", { yPercent: -100 });
-    timeline.from(".div4", { xPercent: -100 });
-
-    ScrollTrigger.create({
-      animation: timeline,
-      trigger: "#container",
-      start: "top top",
-      end: "+=100",
-      scrub: true,
-      pin: true,
-      anticipatePin: 1,
-    });
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    console.log(users);
+    getUsers();
   }, []);
 
   return (
-    <div id="container" className="container" ref={container}>
-      {/* <section>
-        <div ref={ghost} className="sectionInsideDiv">
-          <h1>hello world pinnet</h1>
-        </div>
-        <div ref={scrollSection} className="sectionInsideDiv">
-          <h1>hello world section</h1>
-        </div>
-      </section> */}
+    <div id="container" className="container">
       <div className="div1"> hello world</div>
     </div>
   );
